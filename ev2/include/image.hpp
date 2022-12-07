1e7bb0d33d39ad5bcecfdc83c6080b514eb55cad
Hunter Borlik <hborlik@users.noreply.github.com>
2022-12-07 14:44:03 -0800

    cpu image memory loading

--- ev2/include/image.hpp
+++ ev2/include/image.hpp
@@ -10,14 +10,24 @@
 #define EV2_IMAGE_HPP
 
 #include <utility>
-
+#include <string>
+#include <memory>
+#include <cmath>
 #include <stdint.h>
 
+#include <util.h>
+
 namespace ev2 {
+
+inline int mip_count(int width, int height, int depth = 1) noexcept {
+    return 1 + std::floor(std::log2(util::vmax(width, height, depth)));
+}
 
 class Image {
 public:
     Image() = default;
+
+    ~Image();
 
     Image(const Image& o) noexcept {*this = o;}
     Image(Image&& o) noexcept {*this = std::move(o);}
@@ -25,16 +35,57 @@ public:
     Image& operator=(const Image& o) noexcept;
     Image& operator=(Image&& o) noexcept;
 
-    int get_width()  const noexcept {return width;}
+    int get_width()  const noexcept {return m_width;}
 
-    int get_height()  const noexcept {return height;}
+    int get_height()  const noexcept {return m_height;}
+
+    /**
+     * @brief The number of color channels
+     * 
+     * @return int 
+     */
+    int get_comp() const noexcept {return m_comp;}
+
+    /**
+     * @brief bytes per color channel
+     * 
+     * @return int 
+     */
+    int get_pf() const noexcept {return m_pf;}
+
+    /**
+     * @brief 
+     * 
+     * @return std::size_t 
+     */
+    std::size_t memory_requirement() const noexcept {return m_height*m_width*m_comp*m_pf;}
+
+    uint8_t* data() noexcept {return m_texels;}
+
+    const uint8_t* data() const noexcept {return m_texels;}
+
+    /**
+     * @brief Set the image object, taking ownership of the allocated memory
+     * 
+     * @param width 
+     * @param height 
+     * @param n_comps   number of channels in the image
+     * @param pf        bytes per pixel
+     * @param data 
+     */
+    void set_image(int width, int height, int n_comps, int pf, uint8_t* data);
 
 private:
-    uint8_t* texels = nullptr;
-    int height = -1, width = -1;
+    uint8_t* m_texels = nullptr;
+    int m_height = -1, m_width = -1;
+    int m_comp = -1, m_pf = -1;
 
     Image* next = nullptr;
 };
+
+std::unique_ptr<Image> load_image(const std::string& path);
+
+std::unique_ptr<Image> load_image_16(const std::string& path);
 
 }
 
---
+++ ev2/src/image.cpp
@@ -0,0 +1,74 @@
+#include <stdexcept>
+
+#include <image.hpp>
+#include <renderer/ev_gl.h>
+#include <stb_image.h>
+
+namespace ev2 {
+
+Image::~Image() {
+    if (m_texels)
+        delete m_texels;
+}
+
+void Image::set_image(int width, int height, int n_comps, int pf, uint8_t* data) {
+    if (m_texels)
+        delete m_texels;
+    m_width     = width;
+    m_height    = height;
+    m_comp      = n_comps;
+    m_pf        = pf;
+    m_texels    = data;
+}
+
+std::unique_ptr<Image> load_image(const std::string& path) {
+    int w, h, ncomps;
+    stbi_set_flip_vertically_on_load(false);
+    uint8_t* data = stbi_load(path.c_str(), &w, &h, &ncomps, 0);
+
+    if (data) {
+        if (ncomps > 4) {
+            std::cerr << "Failed to load texture " + path << "invalid ncomps " << ncomps << std::endl;
+            stbi_image_free(data);
+            throw std::runtime_error{"Failed to load texture " + path};
+        }
+        std::unique_ptr<Image> image = std::make_unique<Image>();
+
+        // allow image to keep the memory (will free)
+        image->set_image(w, h, ncomps, 1, data);
+
+        std::cout << "Loaded texture " << path << std::endl;
+        // stbi_image_free(data);
+        return image;
+    } else {
+        std::cerr << "Failed to load texture " << path << std::endl;
+        throw std::runtime_error{"Failed to load texture " + path};
+    }
+}
+
+std::unique_ptr<Image> load_image_16(const std::string& path) {
+    int w, h, ncomps;
+    stbi_set_flip_vertically_on_load(false);
+    uint16_t* data = stbi_load_16(path.c_str(), &w, &h, &ncomps, 0);
+
+    if (data) {
+        if (ncomps > 4) {
+            std::cerr << "Failed to load texture " + path << "invalid ncomps " << ncomps << std::endl;
+            stbi_image_free(data);
+            throw std::runtime_error{"Failed to load texture " + path};
+        }
+        std::unique_ptr<Image> image = std::make_unique<Image>();
+
+        // allow image to keep the memory (will free)
+        image->set_image(w, h, ncomps, 2, (uint8_t*)data);
+
+        std::cout << "Loaded texture " << path << std::endl;
+        // stbi_image_free(data);
+        return image;
+    } else {
+        std::cerr << "Failed to load texture " << path << std::endl;
+        throw std::runtime_error{"Failed to load texture " + path};
+    }
+}
+
+}
